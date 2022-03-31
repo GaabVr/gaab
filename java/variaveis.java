202b88736f9f1275af0f9c9b721e9daa1e26c965
GaabVr <99880756+GaabVr@users.noreply.github.com>
2022-03-31 15:01:28 -0300

    Add files via upload

---
+++ java/variaveis.java
@@ -0,0 +1,15 @@
+public class variaveis {
+    public static void main(String[] args) throws Exception {
+        // tipo nome
+        // variaveis inteiras
+        byte numero = 125;
+        short numeropq = 32;
+        System.out.println(numero);
+        System.out.println(numeropq);
+        // variaveis de ponto flutuante
+        float n = 2.0f;
+        System.out.println(n);
+    }
+
+    
+}
